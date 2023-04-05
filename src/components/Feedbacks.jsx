075b524ced42336aa49ddb9164c89490dbb42c24
OPDhaker <OPDhaker2007@gmail.com>
2023-04-05 17:03:13 +0530

    Interview_UPDATE

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -34,7 +34,7 @@ const Feedbacks = () => {
         <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
             <motion.div variants={textVariant()}>
             <p className={styles.sectionSubText}>What others say</p>
-            <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+            <h2 className={styles.sectionHeadText}>Interviews.</h2>
             </motion.div>
         </div>
         <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
